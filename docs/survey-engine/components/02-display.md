eef3da5ac638fbb76cf83726ec7f9cb4f6577260
Clément Turbelin <clement.turbelin@gmail.com>
2023-07-13 20:26:38 +0200

    add basic text

--- docs/survey-engine/components/02-display.md
+++ docs/survey-engine/components/02-display.md
@@ -7,4 +7,14 @@ sidebar_label: Display Components
 import  { SurveyComponent } from "@site/src/components/survey";
 
 ## TextComponent
+
+Text components can be used to show item with only visual content (like text, image). It can be useful to present section title or note about a survey section
+to the user.
+
+
+### A very basic text item
+<SurveyComponent name="basicText"/>
+
+### More complex component
+
 <SurveyComponent name="textComponent"/>
--- src/components/survey/responses/text.tsx
+++ src/components/survey/responses/text.tsx
@@ -40,6 +40,36 @@ export const TextItem = () => {
  
 export default [
     {
+        name:'basicText',
+        code:'basic',
+        item: ()=> {
+             // --code:basic--
+
+                /**
+                 * The display helper creates a SurveyItem with a list of textual components
+                 * 
+                 * Each component here has the 'text' role and can show a list of text.
+                 * `style` option can be used to customize visual style of the text
+                 */
+
+                return SurveyItems.display({
+                    itemKey:'t1',
+                    parentKey:'test',
+
+                    content: [ // List of texts to show
+                        {
+                            role: 'text',
+                            content:[ 
+                                text("A very intersting text") 
+                            ]
+                        }                       
+                    ]
+                });
+            // --end:basic--
+        }
+        
+    },
+    {
         name:'textComponent',
         item: TextItem
     }
